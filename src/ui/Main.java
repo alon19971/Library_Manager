637e6b4b9171743b4d4c7f61e5e55e2f0edcf394
alon19971 <alonaizin123@gmail.com>
2024-07-25 14:09:34 +0300

    Add Main class to launch the application

---
+++ src/ui/Main.java
@@ -0,0 +1,10 @@
+package ui;
+
+/**
+ * Main class to launch the JavaFX application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainWindow.main(args);
+    }
+}
